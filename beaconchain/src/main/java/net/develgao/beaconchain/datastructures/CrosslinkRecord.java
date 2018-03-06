5aab684b1cf1f350a41dc64a60b041ba9f9e9ffd
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2018-03-06 00:50:14 +0000

    Minor edits with spotlessApply run.

--- beaconchain/src/main/java/net/develgao/beaconchain/datastructures/CrosslinkRecord.java
+++ beaconchain/src/main/java/net/develgao/beaconchain/datastructures/CrosslinkRecord.java
@@ -1,16 +1,17 @@
 package net.develgao.beaconchain.datastructures;
 
 import net.develgao.beaconchain.ethereum.core.Hash;
+
 import org.web3j.abi.datatypes.generated.Int64;
 
 public class CrosslinkRecord {
 
-    private Hash hash;
-    private Int64 dynasty;
-    private Int64 slot;
+  private Hash hash;
+  private Int64 dynasty;
+  private Int64 slot;
 
-    public CrosslinkRecord() {
+  public CrosslinkRecord() {
 
-    }
+  }
 
 }
