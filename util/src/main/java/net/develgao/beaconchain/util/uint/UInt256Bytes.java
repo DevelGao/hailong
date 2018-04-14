30514dda46ff5af68c6662a2b99ecc8f6b055f02
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2018-04-14 02:08:20 +0000

    Insert licence boilerplate into all source files

--- util/src/main/java/net/develgao/beaconchain/util/uint/UInt256Bytes.java
+++ util/src/main/java/net/develgao/beaconchain/util/uint/UInt256Bytes.java
@@ -1,13 +1,21 @@
+/*
+ * Copyright 2018 Developer Gao.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package net.develgao.beaconchain.util.uint;
 
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-
-import java.math.BigInteger;
-import java.util.function.BinaryOperator;
-
-import com.google.common.annotations.VisibleForTesting;
 import net.develgao.beaconchain.util.bytes.Bytes3;
 import net.develgao.beaconchain.util.bytes.Bytes32;
 import net.develgao.beaconchain.util.bytes.Bytes32s;
@@ -15,6 +23,11 @@ import net.develgao.beaconchain.util.bytes.BytesValue;
 import net.develgao.beaconchain.util.bytes.BytesValues;
 import net.develgao.beaconchain.util.bytes.MutableBytes3;
 import net.develgao.beaconchain.util.bytes.MutableBytes32;
+
+import java.math.BigInteger;
+import java.util.function.BinaryOperator;
+
+import com.google.common.annotations.VisibleForTesting;
 
 /**
  * Static operations to work on bytes interpreted as 256 bytes unsigned integers.
