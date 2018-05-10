a6afb4f2bff9cd89547bacef291f4582b57c9fd6
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2018-05-10 08:06:00 +0000

    Added DepositData class.

---
+++ beaconchain/src/main/java/net/develgao/beaconchain/datastructures/BeaconChainOperations/DepositData.java
@@ -0,0 +1,15 @@
+package net.develgao.beaconchain.datastructures.BeaconChainOperations;
+
+import net.develgao.beaconchain.util.uint.UInt64;
+
+public class DepositData {
+
+  private DepositParameters depositParameters;
+  private UInt64 value;
+  private UInt64 timestamp;
+
+  public DepositData() {
+
+  }
+
+}
