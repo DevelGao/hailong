37f5d210d971a6a6b00ba841feff79bd5fd2bb72
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2018-09-16 10:54:59 +0000

    Chain Storage Integration AlifTest

--- services/chainstorage/src/main/java/tech/devgao/artemis/services/chainstorage/ChainStorageService.java
+++ services/chainstorage/src/main/java/tech/devgao/artemis/services/chainstorage/ChainStorageService.java
@@ -18,10 +18,11 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import tech.devgao.artemis.services.ServiceInterface;
 import tech.devgao.artemis.storage.ChainStorage;
+import tech.devgao.artemis.storage.ChainStorageServer;
 
 public class ChainStorageService implements ServiceInterface {
   private EventBus eventBus;
-  private ChainStorage chainStore;
+  private ChainStorageServer chainStore;
   private static final Logger LOG = LogManager.getLogger();
 
   public ChainStorageService() {}
@@ -29,7 +30,7 @@ public class ChainStorageService implements ServiceInterface {
   @Override
   public void init(EventBus eventBus) {
     this.eventBus = eventBus;
-    this.chainStore = new ChainStorage(this.eventBus);
+    this.chainStore = ChainStorage.Create(ChainStorageServer.class, eventBus);
     this.eventBus.register(this);
   }
 
