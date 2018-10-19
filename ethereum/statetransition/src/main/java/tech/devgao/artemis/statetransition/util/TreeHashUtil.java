e9978f84fbce8546d0571a4823ff7aea58829b3b
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2018-10-19 22:59:04 +0000

    added plumbing logic for Validator registry and shuffling seed data 1146763

--- ethereum/statetransition/src/main/java/tech/devgao/artemis/statetransition/util/TreeHashUtil.java
+++ ethereum/statetransition/src/main/java/tech/devgao/artemis/statetransition/util/TreeHashUtil.java
@@ -18,16 +18,39 @@ import net.develgao.cava.bytes.Bytes;
 import net.develgao.cava.bytes.Bytes32;
 import net.develgao.cava.ssz.SSZ;
 import tech.devgao.artemis.datastructures.state.Validator;
+import tech.devgao.artemis.statetransition.BeaconState;
 
+/** This class is a collection of tree hash root convenience methods */
 public final class TreeHashUtil {
 
-  private TreeHashUtil() {}
-
+  /**
+   * Calculate the hash tree root of the provided value
+   *
+   * @param value
+   * @return
+   */
   public static Bytes32 hash_tree_root(Bytes value) {
     return SSZ.hashTreeRoot(value);
   }
 
+  /**
+   * Calculate the hash tree root of the list of validators provided
+   *
+   * @param validators
+   * @return
+   */
   public static Bytes32 hash_tree_root(List<Validator> validators) {
+    // TODO
+    return Bytes32.ZERO;
+  }
+
+  /**
+   * Calculate the hash tree root of the BeaconState provided
+   *
+   * @param state
+   * @return
+   */
+  public static Bytes32 hash_tree_root(BeaconState state) {
     // TODO
     return Bytes32.ZERO;
   }
