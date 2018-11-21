f3e3ffec980f52e843ae71a9882e696ff87204ed
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2018-11-21 05:17:14 +0000

    Fix final updates openrc

--- ethereum/statetransition/src/main/java/tech/devgao/artemis/statetransition/util/BeaconStateUtil.java
+++ ethereum/statetransition/src/main/java/tech/devgao/artemis/statetransition/util/BeaconStateUtil.java
@@ -35,7 +35,7 @@ import static tech.devgao.artemis.datastructures.Constants.WHISTLEBLOWER_REWARD_
 import static tech.devgao.artemis.datastructures.Constants.WITHDRAWABLE;
 import static tech.devgao.artemis.datastructures.Constants.ZERO_HASH;
 import static tech.devgao.artemis.statetransition.util.TreeHashUtil.hash_tree_root;
-import static tech.devgao.artemis.statetransition.util.TreeHashUtil.validatorListHashTreeRoot;
+import static tech.devgao.artemis.statetransition.util.TreeHashUtil.integerListHashTreeRoot;
 import static tech.devgao.artemis.util.bls.BLSAggregate.bls_aggregate_pubkeys;
 import static tech.devgao.artemis.util.bls.BLSVerify.bls_verify;
 import static tech.devgao.artemis.util.bls.BLSVerify.bls_verify_multiple;
@@ -161,8 +161,8 @@ public class BeaconStateUtil {
     }
 
     Bytes32 genesis_active_index_root =
-        validatorListHashTreeRoot(
-            ValidatorsUtil.get_active_validators(
+        integerListHashTreeRoot(
+            ValidatorsUtil.get_active_validator_indices(
                 state.getValidator_registry(), UnsignedLong.valueOf(GENESIS_EPOCH)));
     for (Bytes32 root : state.getLatest_index_roots()) {
       root = genesis_active_index_root;
