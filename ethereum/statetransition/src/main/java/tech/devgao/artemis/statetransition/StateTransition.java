01ea88759a520d4842c9d674cc6074914c90ded3
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2019-06-06 13:16:19 -0500

    adding additional filters to flow and caching of committees Viewer

--- ethereum/statetransition/src/main/java/tech/devgao/artemis/statetransition/StateTransition.java
+++ ethereum/statetransition/src/main/java/tech/devgao/artemis/statetransition/StateTransition.java
@@ -29,7 +29,6 @@ import tech.devgao.artemis.statetransition.util.BlockProcessingException;
 import tech.devgao.artemis.statetransition.util.BlockProcessorUtil;
 import tech.devgao.artemis.statetransition.util.EpochProcessingException;
 import tech.devgao.artemis.statetransition.util.EpochProcessorUtil;
-import tech.devgao.artemis.statetransition.util.PreProcessingUtil;
 import tech.devgao.artemis.util.alogger.ALogger;
 
 public class StateTransition {
@@ -48,6 +47,7 @@ public class StateTransition {
       throws StateTransitionException {
 
     cache_state(state);
+    // Client specific optimization
 
     if (state.getSlot().compareTo(UnsignedLong.valueOf(Constants.GENESIS_SLOT)) > 0
         && state
@@ -58,9 +58,6 @@ public class StateTransition {
       epochProcessor(state, block);
     }
 
-    // Client specific optimization
-    preProcessor(state);
-
     slotProcessor(state);
 
     if (block != null) {
@@ -69,11 +66,6 @@ public class StateTransition {
 
     // Client specific optimization
     state.invalidateCache();
-  }
-
-  protected void preProcessor(BeaconStateWithCache state) {
-    // calculate currentBeaconProposerIndex
-    PreProcessingUtil.cacheCurrentBeaconProposerIndex(state);
   }
 
   /**
