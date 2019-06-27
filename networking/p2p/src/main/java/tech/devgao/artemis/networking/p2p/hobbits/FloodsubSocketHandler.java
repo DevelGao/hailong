f840e31d791e598d620f63f49bea5a8ead524cd9
Tiancai Gao <68060575+DevelGao@users.noreply.github.com>
2019-06-27 12:22:04 -0400

    Implement rpc replyAttestation MyPythonProjects

--- networking/p2p/src/main/java/tech/devgao/artemis/networking/p2p/hobbits/FloodsubSocketHandler.java
+++ networking/p2p/src/main/java/tech/devgao/artemis/networking/p2p/hobbits/FloodsubSocketHandler.java
@@ -22,6 +22,7 @@ import org.apache.tuweni.plumtree.State;
 import tech.devgao.artemis.networking.p2p.hobbits.gossip.GossipMessage;
 import tech.devgao.artemis.storage.ChainStorageClient;
 import tech.devgao.artemis.util.alogger.ALogger;
+import tech.devgao.artemis.util.bls.BLSSignature;
 
 /** TCP persistent connection handler for hobbits messages. */
 public final class FloodsubSocketHandler extends AbstractSocketHandler {
@@ -47,11 +48,11 @@ public final class FloodsubSocketHandler extends AbstractSocketHandler {
         peer.setPeerGossip(gossipMessage.body());
         String[] attributes = gossipMessage.getAttributes().split(",");
         if (attributes[0].equalsIgnoreCase("ATTESTATION")) {
-          Bytes32 root = Bytes32.wrap(gossipMessage.body());
-          this.sendGetAttestations(root);
+          BLSSignature signature = BLSSignature.fromBytes(gossipMessage.body());
+          this.sendGetAttestations(signature);
         } else if (attributes[0].equalsIgnoreCase("BLOCK")) {
-          Bytes32 root = Bytes32.wrap(gossipMessage.body());
-          this.sendGetBlockBodies(root);
+          Bytes32 blockRoot = Bytes32.wrap(gossipMessage.body());
+          this.sendGetBlockBodies(blockRoot);
         }
         receivedMessages.put(key, true);
       }
